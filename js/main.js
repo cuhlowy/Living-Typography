7189092ebe716a3fcf8c5c5bbcb1192f16bf6a12
cuhlowy <chin1604.cc@gmail.com>
2025-03-17 19:38:19 -0500

    copy and paste starter site

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 // JS scripts placed here
+const text = document.getElementById("text");
+
+// Growing Effect on Click
+text.addEventListener("click", () => {
+    text.classList.toggle("grow");
+});
+
+// Wind Effect (Mouse Move)
+document.addEventListener("mousemove", (event) => {
+    let x = event.clientX / window.innerWidth - 0.5;
+    text.style.transform = `rotate(${x * 10}deg)`;
+});
+
+// Rain Effect (Fade Letters on Click)
+text.addEventListener("dblclick", () => {
+    text.style.opacity = "0.5";
+    setTimeout(() => text.style.opacity = "1", 2000);
+});
